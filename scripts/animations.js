3f34cad3d0c273c5516bef5beab44e8583d1d114
Evan Martinez <evandros5@yahoo.com>
2025-02-16 23:35:43 -0800

    Fixed

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,5 +1,22 @@
+// Topbar animation
+const navHolder = document.getElementById('nav-holder');
+
+let isShifted = false
+
+// Banner grow
 const banner = document.getElementById('banner-img');
 
-window.addEventListener('scroll', function() {    
+window.addEventListener('scroll', function() {
+    
+    // Banner update
     banner.style.width = 100 + (0.2 * window.scrollY) + "%";
+
+    // Nav update
+    let rect = navHolder.getBoundingClientRect();
+    
+    if (rect.top <= 0 && !isShifted) {
+
+    } else if (rect.top > 0 && isShifted) {
+
+    }
 });
